11f327ca5642406a473b7eebbc73b4a77a9a2bc0
Samuel Oliveira <samueloliveira900@gmail.com>
2024-12-15 20:04:38 -0300

    feat: adicionado texto na validção e titulo na pagina com flavicon

--- main.js
+++ main.js
@@ -32,7 +32,9 @@ let random1 = Math.floor(Math.random() * 100) + 1;
 
 let number1 = document.getElementById('number1');
 let number2 = document.getElementById('number2');
-input_result = document.getElementById('result');
+let resposta_correta = document.getElementById('correta');
+let resposta_incorreta = document.getElementById('incorreta');
+let input_result = document.getElementById('result');
 result = random + random1;
 
 number1.innerHTML = random
@@ -42,11 +44,18 @@ number2.innerHTML = random1
   E Verde quando correta
  */
 input_result.addEventListener("input", function (e) {
+  resposta_correta.style.display = 'none'
+  resposta_incorreta.style.display = 'none'
+
+
   if(this.value != result){
     this.style.border = '2px solid red'
+    resposta_incorreta.style.display = 'block'
   }
   else{
     this.style.border = '2px solid green'
+    resposta_correta.style.display = 'block'
+
   }
 });
 
